6434060373d7ad5bc8f9a7155534cb6083a225b9
simensgreen <simensgreen007@gmail.com>
2021-02-21 02:48:31 +0300

    parse some data

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+mod geom;
+
 #[cfg(test)]
 mod tests {
+    use crate::geom::OldFile;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let file = OldFile::read("TU204Z.DAT");
+        println!("{:?}", file)
     }
 }
+
