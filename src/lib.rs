d1885b7a8837028687c89abf2b986f973d93d035
simensgreen <simensgreen007@gmail.com>
2021-02-21 16:15:19 +0300

    Revert "parse some data"
    This reverts commit 6434060373d7ad5bc8f9a7155534cb6083a225b9.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,7 @@
-mod geom;
-
 #[cfg(test)]
 mod tests {
-    use crate::geom::OldFile;
-
     #[test]
     fn it_works() {
-        let file = OldFile::read("TU204Z.DAT");
-        println!("{:?}", file)
+        assert_eq!(2 + 2, 4);
     }
 }
-
